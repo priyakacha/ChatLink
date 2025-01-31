49f95298696ffbe7f00b6b5cfad00385586a865f
priyakacha <priyakacha16@gmail.com>
2025-01-31 17:44:04 +0530

    ChatLink

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,9 +1,60 @@
+import { useState } from "react";
 import "./chat.css";
+import EmojiPicker from "emoji-picker-react";
 
 const Chat = () => {
+    const [open , setOpen] = useState(false);
+    const [text , setText] = useState("");
+
+    const handleEmoji = (e) => {
+        setText((prev) => prev + e.emoji);
+        setOpen(false);
+    }
     return (
-        <div className="list"> Chat</div>
+        <div className="chat">
+            <div className="top">
+                <div className="user">
+                   <img src="public/Talk with your Buddy.png" alt="" />
+                   <div className="texts">
+                    <span>Priya Kacha</span>
+                    <p>i have students i have learning computer lanaguages</p>
+                   </div>
+                </div>
+
+                <div className="icons">
+                       <img src="./phone.png" alt="" />
+                       <img src="./video.png" alt="" />
+                       <img src="./info.png" alt="" />
+                </div>
+
+            </div>  
+            <div className="center"></div>
+            <div className="bottom">
+            <div className="icons">
+                <img src="./img.png" alt="" />
+                <img src="./camera.png" alt="" />
+                <img src="./mic.png" alt="" />
+            </div>
+
+            <input type="text" 
+             placeholder="Type a Message..."
+             value={text}
+             onChange={(e) => setText(e.target.value)}
+             />
+
+            <div className="emoji">
+              <img src="./emoji2.png" 
+                alt=""
+                onClick={() => setOpen((prev) => !prev)}
+              />
+              <div className="picker">
+              <EmojiPicker open={open} onEmojiClick={handleEmoji}/>
+              </div>
+            </div>
+            <button className="sendButton">Send</button>
+            </div>   
+         </div>
     )
 }
 
-export default Chat ;
+export default Chat ; 
