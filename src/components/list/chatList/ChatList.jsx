11de2e3ac6506d70371a90e33fdbf16fcedcafc4
priyakacha <priyakacha16@gmail.com>
2025-02-05 16:45:45 +0530

    ChatLink

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./chatList.css";
+import AddUser from "./addUser/AddUser";
 
 const ChatList = () => {
     const [addMode , setAddMode] = useState(false);
@@ -43,6 +44,7 @@ const ChatList = () => {
                     <p>Hello Universe</p>
                 </div>
             </div>
+          { addMode && <AddUser/>}
         </div>
     )
 }
