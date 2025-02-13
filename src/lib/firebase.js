2a4ba3b626e72f4e42ff37231ba1af5a9d2484ee
priyakacha <priyakacha16@gmail.com>
2025-02-13 16:32:47 +0530

    ChatLink

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,17 +1,17 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth";
-// import { getFirestore} from "firebase/firestore";
+import { getFirestore} from "firebase/firestore";
 
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_API_KEY,
-//   authDomain: "chatlink-82595.firebaseapp.com",
-//   projectId: "chatlink-82595",
-//   storageBucket: "chatlink-82595.firebasestorage.app",
-//   messagingSenderId: "93323804411",
-//   appId: "1:93323804411:web:9425180c589eb94ccbba5e"
-// };
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: "chatlink-82595.firebaseapp.com",
+  projectId: "chatlink-82595",
+  storageBucket: "chatlink-82595.firebasestorage.app",
+  messagingSenderId: "93323804411",
+  appId: "1:93323804411:web:9425180c589eb94ccbba5e"
+};
 
-// const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-// export const auth = getAuth()
-// export const db = getFirestore()
+export const auth = getAuth()
+export const db = getFirestore()
